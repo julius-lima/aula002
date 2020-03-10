459b352318de4669b71481c44e809cd4f8e64be2
julius-lima <juliobrasilg12@gmail.com>
2020-03-09 21:23:31 -0300

    criar o projeto

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Faisp.OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculo Recisão!");
+            Console.WriteLine("Informe a data de contratação (dd/MM/aaaa): ");            
+            var dataContratacao = Console.ReadLine();
+
+            DateTime dataContratacaoConvertida;
+
+            var converteu = DateTime.TryParse(dataContratacao, out dataContratacaoConvertida);
+
+            if(converteu == true)
+            {
+                Console.WriteLine("Você informou a data: {0}", dataContratacao);
+            }
+
+            else
+            {
+                Console.WriteLine("Data inválida!");
+            }
+            Console.ReadKey();
+        }
+    }   
+
+}
